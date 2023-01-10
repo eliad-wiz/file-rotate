774f3a91399bc910f256c8eaa6601ba687b2f5f9
Jon Doron <jond@wiz.io>
2023-01-10 15:41:53 +0200

    Move compression to a forked process
    Signed-off-by: Jon Doron <jond@wiz.io>

--- src/compression.rs
+++ src/compression.rs
@@ -1,5 +1,7 @@
 //! Compression - configuration and implementation
 use flate2::write::GzEncoder;
+use fork::{fork, Fork};
+use rustix::process;
 use std::{
     fs::{self, File, OpenOptions},
     io,
@@ -16,9 +18,7 @@ pub enum Compression {
     OnRotate(usize),
 }
 
-pub(crate) fn compress(path: &Path) -> io::Result<PathBuf> {
-    let dest_path = PathBuf::from(format!("{}.gz", path.display()));
-
+fn compress_child_helper(path: &Path, dest_path: &PathBuf) -> io::Result<()> {
     let mut src_file = File::open(path)?;
     let dest_file = OpenOptions::new()
         .write(true)
@@ -32,6 +32,43 @@ pub(crate) fn compress(path: &Path) -> io::Result<PathBuf> {
     io::copy(&mut src_file, &mut encoder)?;
 
     fs::remove_file(path)?;
+    Ok(())
+}
 
-    Ok(dest_path)
+pub(crate) fn compress(path: &Path) -> io::Result<PathBuf> {
+    let dest_path = PathBuf::from(format!("{}.gz", path.display()));
+
+    match fork() {
+        Ok(Fork::Parent(child)) => {
+            let child = unsafe {
+                process::Pid::from_raw(child as u32)
+                    .ok_or(io::Error::new(io::ErrorKind::Other, "Invalid child pid"))?
+            };
+            let wait_status = process::waitpid(Some(child), process::WaitOptions::empty())
+                .map_err(|_| io::Error::new(io::ErrorKind::Other, "WaitPid failed"))?
+                .ok_or(io::Error::new(
+                    io::ErrorKind::Other,
+                    "Child sbom writer unexpected return",
+                ))?;
+            let exit_status = wait_status.exit_status().ok_or(io::Error::new(
+                io::ErrorKind::Other,
+                "Failed getting child exit code",
+            ))?;
+            match exit_status {
+                0 => Ok(dest_path),
+                _ => Err(io::Error::new(
+                    io::ErrorKind::Other,
+                    "Compress child failed",
+                )),
+            }
+        }
+        Ok(Fork::Child) => {
+            std::process::exit(
+                compress_child_helper(path, &dest_path)
+                    .and(Ok(0))
+                    .unwrap_or(1),
+            );
+        }
+        Err(_) => Err(io::Error::new(io::ErrorKind::Other, "Fork failed")),
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -286,7 +286,6 @@
     missing_docs,
     trivial_casts,
     trivial_numeric_casts,
-    unsafe_code,
     unused_import_braces,
     unused_qualifications
 )]
